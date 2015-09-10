4f3b184fc66ad025209d6270f68d6365efeec2b0
Yulia Markina <jmarkina@gmail.com>
2015-09-10 13:02:48 -0700

    add 7th CoderByte challenge solution

--- 07simpleSymbols.js
+++ 07simpleSymbols.js
@@ -1,4 +1,13 @@
-//TODO: continue looping even first index is true
+// Using the JavaScript language, have the function SimpleSymbols(str) 
+// take the str parameter being passed and determine if it is an 
+// acceptable sequence by either returning the string true or false. 
+// The str parameter will be composed of + and = symbols with several 
+// letters between them (ie. ++d+===+c++==a) and for the string to be true 
+// each letter must be surrounded by a + symbol. So the string to the 
+// left would be false. The string will not be empty and will have at 
+// least one letter. 
+
+
 function CheckPlus(str) {
 	var indexOfStrToCheck = '';
 	var alphabet = 'abcdefghijklmnopqrstuvwxyz';
@@ -10,7 +19,6 @@ function CheckPlus(str) {
 			}
 		}
 	}
-	console.log(indexOfStrToCheck);
 	for (var k=0; k<indexOfStrToCheck.length; k++) {
 		var charIndex = indexOfStrToCheck[k];
 		if (str[charIndex] !== "+") {
