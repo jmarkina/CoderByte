e25e3027348b5f0ace0ce2b5a418039ea73a36fc
Yulia Markina <jmarkina@gmail.com>
2015-09-11 15:09:16 -0700

    rename files

---
+++ 17.arrayAddition.js
@@ -0,0 +1,20 @@
+// Using the JavaScript language, have the function ArrayAdditionI(arr) 
+// take the array of numbers stored in arr and return the string 
+// true if any combination of numbers in the array can be added
+// up to equal the largest number in the array, otherwise return 
+// the string false. For example: if arr contains [4, 6, 23, 10, 1, 3] 
+// the output should return true because 4 + 6 + 10 + 3 = 23. 
+// The array will not be empty, will not contain all the same elements, 
+// and may contain negative numbers. 
+
+function ArrayAddition(arr) {
+	var sortedArr = arr.sort(function(a,b) { return a - b; });
+	var sum = 0;
+	var biggestNum = sortedArr.slice(-1)[0];
+	for (var i=0; i<sortedArr.length - 1; i++) {
+		sum = sum + arr[i]; 
+	}
+	return sum === biggestNum;
+}
+
+ArrayAddition([3,5,-1,8,12]);
